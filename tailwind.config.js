d2bcb0801f99b6ac212c09ddd4eb20e1651cc877
Gabegar <gagarcia190@gmail.com>
2023-09-08 18:48:52 -0400

    updated bg img paths to utilize absolute pathing for builds

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,33 +5,35 @@ export default {
         extend: {
             // * Mobile images
             backgroundImage: {
-                mCuriosity: "url('src/assets/images/mobile/curiosity.jpg')",
-                mDeepEarth: "url('src/assets/images/mobile/deep-earth.jpg')",
-                mFishEye: "url('src/assets/images/mobile/fisheye.jpg')",
-                mFromAbove: "url('src/assets/images/mobile/from-above.jpg')",
-                mGrid: "url('src/assets/images/mobile/grid.jpg')",
-                mHero: "url('src/assets/images/mobile/hero.jpg')",
-                mInteractive: "url('src/assets/images/mobile/interactive.jpg')",
+                mCuriosity: "url('/src/assets/images/mobile/curiosity.jpg')",
+                mDeepEarth: "url('/src/assets/images/mobile/deep-earth.jpg')",
+                mFishEye: "url('/src/assets/images/mobile/fisheye.jpg')",
+                mFromAbove: "url('/src/assets/images/mobile/from-above.jpg')",
+                mGrid: "url('/src/assets/images/mobile/grid.jpg')",
+                mHero: "url('/src/assets/images/mobile/hero.jpg')",
+                mInteractive:
+                    "url('/src/assets/images/mobile/interactive.jpg')",
                 mNightArcade:
-                    "url('src/assets/images/mobile/night-arcade.jpg')",
+                    "url('/src/assets/images/mobile/night-arcade.jpg')",
                 mPocketBorealis:
-                    "url('src/assets/images/mobile/pocket-borealis.jpg')",
-                mSoccerTeam: "url('src/assets/images/mobile/soccer-team.jpg')",
+                    "url('/src/assets/images/mobile/pocket-borealis.jpg')",
+                mSoccerTeam: "url('/src/assets/images/mobile/soccer-team.jpg')",
 
                 // * Desktop images
-                dCuriosity: "url('src/assets/images/desktop/curiosity.jpg')",
-                dDeepEarth: "url('src/assets/images/desktop/deep-earth.jpg')",
-                dFishEye: "url('src/assets/images/desktop/fisheye.jpg')",
-                dFromAbove: "url('src/assets/images/desktop/from-above.jpg')",
-                dGrid: "url('src/assets/images/desktop/grid.jpg')",
-                dHero: "url('src/assets/images/desktop/hero.jpg')",
+                dCuriosity: "url('/src/assets/images/desktop/curiosity.jpg')",
+                dDeepEarth: "url('/src/assets/images/desktop/deep-earth.jpg')",
+                dFishEye: "url('/src/assets/images/desktop/fisheye.jpg')",
+                dFromAbove: "url('/src/assets/images/desktop/from-above.jpg')",
+                dGrid: "url('/src/assets/images/desktop/grid.jpg')",
+                dHero: "url('/src/assets/images/desktop/hero.jpg')",
                 dInteractive:
-                    "url('src/assets/images/desktop/interactive.jpg')",
+                    "url('/src/assets/images/desktop/interactive.jpg')",
                 dNightArcade:
-                    "url('src/assets/images/desktop/night-arcade.jpg')",
+                    "url('/src/assets/images/desktop/night-arcade.jpg')",
                 dPocketBorealis:
-                    "url('src/assets/images/desktop/pocket-borealis.jpg')",
-                dSoccerTeam: "url('src/assets/images/desktop/soccer-team.jpg')",
+                    "url('/src/assets/images/desktop/pocket-borealis.jpg')",
+                dSoccerTeam:
+                    "url('/src/assets/images/desktop/soccer-team.jpg')",
             },
             colors: {
                 "primary-white": "hsl(var(--white) / <alpha-value>)",
