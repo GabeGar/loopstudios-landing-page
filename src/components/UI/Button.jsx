3cc29d9bbb90e0f48c28a685eaf1a9b32212bd4d
Gabegar <gagarcia190@gmail.com>
2023-09-09 10:57:24 -0400

    updated font styles on button to match design

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -2,7 +2,7 @@ const Button = ({ classes, onClick, id, children }) => {
     const allClasses = `${classes} ${
         id === "burger" || id === "modalClose"
             ? ""
-            : "border-[.15rem] border-solid border-primary-dark-gray px-10 py-2 tracking-[.3rem]"
+            : "border-[.15rem] border-solid border-primary-dark-gray px-10 py-2 tracking-[.3rem] font-alata uppercase"
     }`;
 
     return (
