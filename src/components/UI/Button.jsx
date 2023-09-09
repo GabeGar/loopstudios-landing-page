4b9192da2ebe8a4a07b52a987d46b8ba0bfa1066
Gabegar <gagarcia190@gmail.com>
2023-09-09 10:09:06 -0400

    updated button styles

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -1,8 +1,8 @@
 const Button = ({ classes, onClick, id, children }) => {
-    const allClasses = `${classes} px-10 py-2 tracking-[.3rem] ${
+    const allClasses = `${classes} ${
         id === "burger" || id === "modalClose"
             ? ""
-            : "border-[.15rem] border-solid border-primary-dark-gray"
+            : "border-[.15rem] border-solid border-primary-dark-gray px-10 py-2 tracking-[.3rem]"
     }`;
 
     return (
